27c07cf91967d5b0282cb55c7d203f13ec50b349
Tushar Maheshwari <tushar27192@gmail.com>
2016-01-23 15:56:34 +0530

    Select SplitViewCommand on click.

---
+++ KideMediaPlayer/js/splitViewCommand.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    window.somethingClicked = WinJS.UI.eventHandler(function (ev) {
+        var previouslySelected = ev.target.parentElement.querySelector("[isSelected]");
+        if (null !== previouslySelected) {
+            previouslySelected.removeAttribute("isSelected");
+        }
+        ev.target.setAttribute("isSelected");
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll("#app .nav-commands > div")[0].setAttribute("isSelected")
+    });
+
+})();
